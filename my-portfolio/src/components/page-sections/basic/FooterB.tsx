020f7615034e80e07ee42e9ae8c28d72b6e3ad5a
Tea Sallstedt Elming <ts223ir@student.lnu.se>
2025-03-13 11:54:30 +0100

    Basic links to github etc added

---
+++ my-portfolio/src/components/page-sections/basic/FooterB.tsx
@@ -0,0 +1,63 @@
+/** @format */
+import { Nav } from "react-bootstrap"
+import OverlayTrigger from "react-bootstrap/OverlayTrigger"
+import Tooltip from "react-bootstrap/Tooltip"
+
+const FooterB = () => {
+	return (
+		<footer className="footerB">
+			{/* LinkedIn */}
+			<Nav.Item>
+				<OverlayTrigger
+					placement="top"
+					overlay={<Tooltip id="tooltip-linkedIn">LinkedIn</Tooltip>}
+					trigger={["hover", "focus"]}
+				>
+					<Nav.Link
+						href="https://www.linkedin.com/in/tea-elming/"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<i className="bi bi-linkedin"></i>
+					</Nav.Link>
+				</OverlayTrigger>
+			</Nav.Item>
+
+			{/* GitHub */}
+			<Nav.Item>
+				<OverlayTrigger
+					placement="top"
+					overlay={<Tooltip>GitHub</Tooltip>}
+					trigger={["hover", "focus"]}
+				>
+					<Nav.Link
+						href="https://github.com/TeaElming"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<i className="bi bi-github"></i>
+					</Nav.Link>
+				</OverlayTrigger>
+			</Nav.Item>
+
+			{/* CV */}
+			<Nav.Item>
+				<OverlayTrigger
+					placement="top"
+					overlay={<Tooltip>CV</Tooltip>}
+					trigger={["hover", "focus"]}
+				>
+					<Nav.Link
+						href="/TeaElming_GraduateSoftwareDeveloper.pdf"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<i className="bi bi-person-vcard"></i>
+					</Nav.Link>
+				</OverlayTrigger>
+			</Nav.Item>
+		</footer>
+	)
+}
+
+export default FooterB
--- my-portfolio/src/components/page-sections/basic/NavbarB.tsx
+++ my-portfolio/src/components/page-sections/basic/NavbarB.tsx
@@ -1,36 +1,42 @@
 /** @format */
-
 import { Nav } from "react-bootstrap"
 import { NavLink } from "react-router-dom"
+
+import FooterB from "./FooterB"   
 import "./css/Navbar.css"
 
 const NavbarB = () => {
 	return (
 		<div className="navbarB">
-			<Nav className="flex-column">
-				<ul>
-					<li>
-						<Nav.Link as={NavLink} to="/start">
-							Start
-						</Nav.Link>
-					</li>
-					<li>
-						<Nav.Link as={NavLink} to="/education">
-							Education
-						</Nav.Link>
-					</li>
-					<li>
-						<Nav.Link as={NavLink} to="/projects">
-							Projects
-						</Nav.Link>
-					</li>
-					<li>
-						<Nav.Link as={NavLink} to="/about">
-							About
-						</Nav.Link>
-					</li>
-				</ul>
-			</Nav>
+			<div className="nav-items">
+				<Nav className="flex-column">
+					<ul>
+						<li>
+							<Nav.Link as={NavLink} to="/start">
+								Start
+							</Nav.Link>
+						</li>
+						<li>
+							<Nav.Link as={NavLink} to="/education">
+								Education
+							</Nav.Link>
+						</li>
+						<li>
+							<Nav.Link as={NavLink} to="/projects">
+								Projects
+							</Nav.Link>
+						</li>
+						<li>
+							<Nav.Link as={NavLink} to="/about">
+								About
+							</Nav.Link>
+						</li>
+					</ul>
+				</Nav>
+			</div>
+
+			{/* Footer pinned at bottom via flex rules */}
+			<FooterB />
 		</div>
 	)
 }
