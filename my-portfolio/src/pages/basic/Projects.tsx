4cf21a485412ba8a105bd7e955a4d857b0a63ebf
Tea Sallstedt Elming <ts223ir@student.lnu.se>
2025-05-13 08:22:33 +0200

    Projects bare udpated

--- my-portfolio/src/pages/basic/Projects.tsx
+++ my-portfolio/src/pages/basic/Projects.tsx
@@ -1,16 +1,51 @@
 /** @format */
-
-import React from "react"
-
-
+import React, { useState } from "react"
+import ProjectsCarousel from "../../components/projects/project-carousel"
 import "./css/Projects.css"
-import ProjectGrid from "../../components/projects/project-grid"
 
 const ProjectsB: React.FC = () => {
+	const [filter, setFilter] = useState("all")
+
+	const handleFilterClick = (newFilter: string) => {
+		setFilter(newFilter)
+	}
 
 	return (
-		<div className="projectsB-container">
-        <ProjectGrid />
+		<div className="projectsB-root">
+			<div className="projectsB-container">
+				{/* Filter Buttons */}
+				<div className="projectsB-button-section">
+					<button
+						className={filter === "all" ? "active" : ""}
+						onClick={() => handleFilterClick("all")}
+					>
+						View All
+					</button>
+					<button
+						className={filter === "fullstack" ? "active" : ""}
+						onClick={() => handleFilterClick("fullstack")}
+					>
+						Full-Stack
+					</button>
+					<button
+						className={filter === "frontend" ? "active" : ""}
+						onClick={() => handleFilterClick("frontend")}
+					>
+						Front-End
+					</button>
+					<button
+						className={filter === "backend" ? "active" : ""}
+						onClick={() => handleFilterClick("backend")}
+					>
+						Back-End
+					</button>
+				</div>
+
+				{/* Carousel Section */}
+				<div className="projectsB-carousel">
+					<ProjectsCarousel filter={filter} />
+				</div>
+			</div>
 		</div>
 	)
 }
